709b79a7c599c8e6d6ce70e19fab185cab5b0d9a
Ahmed Elaguab <elaguabahmed97@gmail.com>
2021-05-18 19:03:35 +0100

    Add header hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
+
+import { HiMenu, HiX } from 'react-icons/hi';
+
 export const Header = () => {
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  const handleMenuToggle = () => setIsMenuActive((prevState) => !prevState);
+
+  const MenuIcon = isMenuActive ? HiX : HiMenu;
+
   return (
     <header class="bg-white">
       <div className="container">
         <div className="h-16 py-3 px-4 flex items-center border-b border-gray-200">
+          <div>
+            <MenuIcon className="text-3xl mr-3" onClick={handleMenuToggle} />
+          </div>
           <div>
             <a className="text-2xl font-medium" href="/">
               <span>a|e </span>
