fc301ab36669c907216fd9bb5f12514b89292c52
Ahmed Elaguab <elaguabahmed97@gmail.com>
2021-05-19 21:07:27 +0100

    Add intro component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { Header } from './components/Header';
 import { Layout } from './components/Layout';
+import { Intro } from './components/Intro';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-white text-gray-800">
       <Layout>
-        <section className="pt-20">
-          <h1>Hello world!</h1>
-        </section>
+        <main className="pt-20">
+          <Intro />
+        </main>
       </Layout>
     </div>
   );
---
+++ src/components/Intro.jsx
@@ -0,0 +1,20 @@
+import { ReactComponent as IntroImage } from '../assets/intro.svg';
+
+export const Intro = () => {
+  return (
+    <section className="container">
+      <div className="px-6">
+        <div className="flex flex-col md:flex-row md:justify-between py-28 lg:px-16">
+          <h1 className="flex flex-col justify-center text-3xl xl:text-4xl xl:leading-relaxed font-medium leading-relaxed">
+            Welcome to my portfolio. <br className="hidden sm:block" />
+            My name is Ahmed Elaguab, <br className="hidden sm:block" />
+            and I'm a Frontend Developer.
+          </h1>
+          <div className="flex justify-end md:flex-col md:justify-center mt-14 md:mt-0">
+            <IntroImage className="w-48 h-48 xl:w-72 " />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
