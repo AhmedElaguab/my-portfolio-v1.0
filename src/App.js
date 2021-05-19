d7e96eeba5754c83bb8cc38038dbc70e093ec4ca
Ahmed Elaguab <elaguabahmed97@gmail.com>
2021-05-19 20:13:59 +0100

    Add layout component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Header } from './components/Header';
+import { Layout } from './components/Layout';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-white text-gray-800">
-      <Header />
+      <Layout></Layout>
     </div>
   );
 }
