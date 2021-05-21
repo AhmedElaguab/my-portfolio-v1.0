68a00b8af019b15695a9048cfb594562a7d3517b
Ahmed Elaguab <elaguabahmed97@gmail.com>
2021-05-21 18:13:28 +0100

    Add react-router links

--- src/components/NavbarLink.jsx
+++ src/components/NavbarLink.jsx
@@ -1,13 +1,15 @@
+import { NavLink } from 'react-router-dom';
+
 export const NavbarLink = ({ label, linkUrl, onClick }) => {
   return (
     <li>
-      <a
+      <NavLink
         className="flex py-2 px-3 mb-2 lg:mb-0 lg:mr-2 text-xl rounded text-gray-700 hover:text-gray-800 hover:bg-purple-50"
-        href={linkUrl}
+        to={linkUrl}
         onClick={onClick}
       >
         {label}
-      </a>
+      </NavLink>
     </li>
   );
 };
