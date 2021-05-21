acc33e881822c6beee5cdae7d1e5530eb81ded5f
Ahmed Elaguab <elaguabahmed97@gmail.com>
2021-05-21 18:18:10 +0100

    Fix about section paragraphs width

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,12 +4,12 @@ export const About = () => {
       <div className="px-6">
         <div className="py-28">
           <h2 className="text-2xl font-bold">•••About</h2>
-          <p className="mt-6 text-xl text-gray-700 w-9/12">
+          <p className="mt-6 text-xl text-gray-700 md:w-9/12 xl:w-8/12">
             I'm Ahmed, a Frontend Web Developer with a strong desire to learn
             new technologies. Looking for an Frontend Developer or a Web
             Developer role.
           </p>
-          <p className="mt-6 text-xl text-gray-700 w-9/12">
+          <p className="mt-6 text-xl text-gray-700 md:w-9/12 xl:w-8/12">
             I build websites using HTML5, CSS3, JavaScript, jQuery and Sass. I
             can also use React and Vue, the most popular JavaScript library and
             framework.
