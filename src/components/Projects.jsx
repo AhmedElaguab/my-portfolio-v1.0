b306defd15171a9849b8eee2627d0cc20e20bda6
Ahmed Elaguab <elaguabahmed97@gmail.com>
2021-05-24 17:01:18 +0100

    Add project category component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,6 @@
-import { FaLink, FaGithubSquare } from 'react-icons/fa';
+import { ProjectCategory } from './ProjectCategory';
 
-import { projects } from '../projects.data';
-
-import Image from '../assets/bg.png';
+import { PROJECTS } from '../projects.data';
 
 export const Projects = () => {
   return (
@@ -12,48 +10,9 @@ export const Projects = () => {
           <div className="pt-28">
             <div>
               <h2 className="text-2xl font-bold">•••Projects</h2>
-              <div className="mt-6 lg:flex flex-wrap">
-                {projects.map((project) => (
-                  <div className="lg:w-2/4 lg:px-3">
-                    <article className="flex flex-col sm:flex-row  sm:h-40 bg-white mb-8 rounded-md shadow-md overflow-hidden">
-                      <header className="sm:w-4/12 md:w-5/12">
-                        <img
-                          className="flex h-44 sm:h-full w-full object-cover"
-                          src={Image}
-                        />
-                      </header>
-                      <div className="sm:flex sm:flex-col sm:w-8/12 md:w-7/12">
-                        <main className="p-4 flex-grow">
-                          <h3 className="text-xl font-semibold flex-grow">
-                            {project.name}
-                          </h3>
-                          <p className="mt-2 text-gray-600">{project.desc}</p>
-                        </main>
-                        <hr className="border-t border-gray-200 mt-2 sm:mt-0" />
-                        <footer className="flex items-center p-4 sm:py-2">
-                          <div className="felx items-center uppercase font-semibold text-xs tracking-widest mr-3">
-                            <a
-                              className="flex bg-purple-600 text-white hover:bg-purple-700 py-1 px-2 rounded"
-                              href={project.preview}
-                            >
-                              <FaLink className="mr-2 mt-0.5" />
-                              <span>Preview</span>
-                            </a>
-                          </div>
-                          <div className="">
-                            <a
-                              className="flex items-center text-2xl"
-                              href={project.github}
-                            >
-                              <FaGithubSquare />
-                            </a>
-                          </div>
-                        </footer>
-                      </div>
-                    </article>
-                  </div>
-                ))}
-              </div>
+              {PROJECTS.map((projectSet) => (
+                <ProjectCategory {...projectSet} />
+              ))}
             </div>
           </div>
         </div>
